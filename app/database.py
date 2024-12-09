4a0f70a256772a6959aa36d1f701051b71fffc25
Max Ebert <max.ebert@itr-software.de>
2024-12-09 11:35:36 +0100

    update to use psycopg3

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, future=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
