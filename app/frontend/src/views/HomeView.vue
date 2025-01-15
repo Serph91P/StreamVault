30794034a01816515822d57f175971310e167352
Max Ebert <max.ebert@itr-software.de>
2025-01-15 15:56:38 +0100

    add Streamer List

--- app/frontend/src/views/HomeView.vue
+++ app/frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import StreamerList from '../components/StreamerList.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <StreamerList />
   </main>
 </template>
