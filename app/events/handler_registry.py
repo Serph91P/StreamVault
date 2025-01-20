c2d9e92e0a1f96181b9444c796a44135564f324a
Seraph91P <github@mebert-server.de>
2025-01-20 17:02:15 +0100

    positiionargument

--- app/events/handler_registry.py
+++ app/events/handler_registry.py
@@ -51,8 +51,8 @@ class EventHandlerRegistry:
             logger.debug("Setting up stream.online subscription")
             online_sub = await self.eventsub.listen_stream_online(
                 twitch_id,
-                secret=self.settings.EVENTSUB_SECRET,
-                self.handle_stream_online
+                self.handle_stream_online,
+                secret=self.settings.EVENTSUB_SECRET
             )
             logger.info(f"Stream.online subscription created with ID: {online_sub}")
 
