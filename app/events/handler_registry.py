ad3fe1ade4621719bf69b46d0c74ac72e4352ee7
Seraph91P <github@mebert-server.de>
2025-01-20 17:04:54 +0100

    removed secret completely

--- app/events/handler_registry.py
+++ app/events/handler_registry.py
@@ -51,8 +51,7 @@ class EventHandlerRegistry:
             logger.debug("Setting up stream.online subscription")
             online_sub = await self.eventsub.listen_stream_online(
                 twitch_id,
-                self.handle_stream_online,
-                secret=self.settings.EVENTSUB_SECRET
+                self.handle_stream_online
             )
             logger.info(f"Stream.online subscription created with ID: {online_sub}")
 
