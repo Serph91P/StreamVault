1e4fad6d1cd898c1afb9f84f880dc164951a0823
Seraph91P <github@mebert-server.de>
2025-01-21 10:36:43 +0100

    forgot datetim import

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text, Boolean
 from sqlalchemy.sql import func
 from app.database import Base
+from datetime import datetime
 
 class Streamer(Base):
     __tablename__ = "streamers"
