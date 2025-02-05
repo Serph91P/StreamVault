599f6cdc59cd2bd440b9a9187e9a1f2a7b5549e7
Max Ebert <max.ebert@itr-software.de>
2025-02-05 09:47:09 +0100

    feat(main): import APIRoute from fastapi.routing

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request, HTTPException, Depends
 from fastapi.responses import HTMLResponse, Response, FileResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.routing import APIRoute
 from starlette.routing import WebSocketRoute
 from app.routes import streamers, auth
 from app.routes import settings as settings_router
