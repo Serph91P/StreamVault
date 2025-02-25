2a626a079538ffbc29da570477666f2549f516f1
Seraph91P <maximilianebert1991@gmail.com>
2025-02-25 12:03:47 +0100

    feat(view): add StreamerDetailView component to display streamer information and streams

---
+++ app/frontend/src/views/StreamerDetailView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="streamer-detail">
+    <div class="streamer-header" v-if="streamerName">
+      <h1>Streams von {{ streamerName }}</h1>
+    </div>
+    <StreamList />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+import StreamList from '@/components/StreamList.vue'
+
+const route = useRoute()
+const streamerName = computed(() => route.query.name as string)
+</script>
