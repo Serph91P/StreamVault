e58b1b919dbd3b48e330c3936b803f0b1f9b2d47
Seraph91P <maximilianebert1991@gmail.com>
2025-02-27 18:14:38 +0100

    feat(models): reorder import statements for improved readability and consistency

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text, Boolean
+from sqlalchemy import Column, String, Integer, Boolean, DateTime, ForeignKey, UniqueConstraint
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from app.database import Base
