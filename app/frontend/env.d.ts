4280a8261fdad2e6614a1c82626e6dedc3256d37
/ <github@mebert-server.de>
2025-05-25 17:01:29 +0200

    feat(cleanup): add initial cleanup migrations script

--- app/frontend/env.d.ts
+++ app/frontend/env.d.ts
@@ -1,4 +1,11 @@
 /// <reference types="vite/client" />
+/// <reference types="vue/ref-macros" />
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
 
 interface ImportMetaEnv {
     readonly VITE_APP_TITLE: string
