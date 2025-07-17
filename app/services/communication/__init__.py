b5ae0442b97f70b67caa653e25255eb21e5f466d
Seraph91P <github@mebert-server.de>
2025-07-17 19:33:04 +0200

    fix: Remove EnhancedPushService import for consistency in communication services

--- app/services/communication/__init__.py
+++ app/services/communication/__init__.py
@@ -5,11 +5,10 @@ Contains notification and communication services.
 """
 
 from .webpush_service import ModernWebPushService, webpush_service
-from .enhanced_push_service import EnhancedPushService, enhanced_push_service  
+from .webpush_service import ModernWebPushService  
 from .websocket_manager import ConnectionManager, websocket_manager
 
 __all__ = [
     'ModernWebPushService', 'webpush_service',
-    'EnhancedPushService', 'enhanced_push_service', 
     'ConnectionManager', 'websocket_manager'
 ]
