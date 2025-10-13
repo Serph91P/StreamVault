f0d4841b3246be7b052dd15c732dd1cddae48f61
Seraph91P <github@mebert-server.de>
2025-10-13 11:10:19 +0200

    feat: add configuration module for StreamVault with constants import

---
+++ app/config/__init__.py
@@ -0,0 +1,21 @@
+"""
+Configuration module for StreamVault
+"""
+
+from .constants import (
+    ASYNC_DELAYS,
+    RETRY_CONFIG,
+    TIMEOUTS,
+    CACHE_CONFIG,
+    FILE_SIZE_THRESHOLDS,
+    METADATA_CONFIG
+)
+
+__all__ = [
+    'ASYNC_DELAYS',
+    'RETRY_CONFIG',
+    'TIMEOUTS',
+    'CACHE_CONFIG',
+    'FILE_SIZE_THRESHOLDS',
+    'METADATA_CONFIG'
+]
